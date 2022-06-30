7b81c189026d70973be7c811f4b317732f8deead
Joao Pedro Thome <joaothome@vipcommerce.com.br>
2022-06-30 19:27:59 -0300

    adicionando changeDetection onPush nos componentes

--- src/products/containers/product-item/product-item.component.ts
+++ src/products/containers/product-item/product-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { ChangeDetectionStrategy, Component, OnInit } from "@angular/core";
 
 import { Store } from "@ngrx/store";
 import { Observable } from "rxjs/Observable";
@@ -10,6 +10,7 @@ import { Topping } from "../../models/topping.model";
 
 @Component({
   selector: "product-item",
+  changeDetection: ChangeDetectionStrategy.OnPush,
   styleUrls: ["product-item.component.scss"],
   template: `
     <div class="product-item">
--- src/products/containers/products/products.component.ts
+++ src/products/containers/products/products.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { ChangeDetectionStrategy, Component, OnInit } from "@angular/core";
 
 import * as fromStore from "../../store";
 import { Pizza } from "../../models/pizza.model";
@@ -7,6 +7,7 @@ import { Observable } from "rxjs/Observable";
 
 @Component({
   selector: "products",
+  changeDetection: ChangeDetectionStrategy.OnPush,
   styleUrls: ["products.component.scss"],
   template: `
     <div class="products">
